10d0b01d44801b41a449c9cc8379a85f28add93c
OliverZShen96 <oliver.z.shen.96@gmail.com>
2017-02-19 00:23:03 +1100

    Formatting, Removed unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,11 @@
 import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpParams;
-import org.json.*;
 
+@SuppressWarnings("deprecation")
 public class Main {
 	
 	
@@ -34,11 +24,11 @@ public class Main {
 		
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 		
-    	String data = "";
-    	String line = "";
-    	while ((line = rd.readLine()) != null) {
-    		data = data + line;
-    		System.out.println(line);
-    	}
+		String data = "";
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			data = data + line;
+			System.out.println(line);
+		}
 	}
 }
